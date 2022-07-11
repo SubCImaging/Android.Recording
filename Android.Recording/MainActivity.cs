14a356a383466ab1f4b956c7a97b31b1ff7cca42
Adam <apr@subccontrol.com>
2022-07-11 09:55:35 -0230

    Working preview

--- Android.Recording/MainActivity.cs
+++ Android.Recording/MainActivity.cs
@@ -1,25 +1,139 @@
 ﻿using Android.App;
+using Android.Hardware.Camera2;
 using Android.OS;
 using Android.Runtime;
 using AndroidX.AppCompat.App;
+using System.Threading.Tasks;
+using System;
+using Android.Views;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
 
 namespace Android.Recording
 {
     [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
     public class MainActivity : AppCompatActivity
     {
-        protected override void OnCreate(Bundle savedInstanceState)
+        public static Task<CameraDevice> OpenCameraAsync(string cameraId, CameraManager cameraManager)
         {
-            base.OnCreate(savedInstanceState);
-            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
-            // Set our view from the "main" layout resource
-            SetContentView(Resource.Layout.activity_main);
+            // create a new background thread to open the camera on
+            var backgroundThread = new HandlerThread("CameraBackground");
+            backgroundThread.Start();
+            var handler = new Android.OS.Handler(backgroundThread.Looper);
+
+            return OpenCameraAsync(cameraId, cameraManager, new CameraStateCallback(), handler);
         }
+
+        /// <summary>
+        /// Gets a <see cref="CameraDevice"/> from a <see cref="Context"/> and a cameraId.
+        /// </summary>
+        /// <param name="cameraId">Camera Id.</param>
+        /// <param name="cameraManager">A <see cref="CameraManager"/>.</param>
+        /// <param name="stateCallback">A <see cref="CameraStateCallback"/>.</param>
+        /// <param name="backgroundHandler">A background handler.</param>
+        /// <returns>A task of CameraDevice.</returns>
+        public static async Task<CameraDevice> OpenCameraAsync(string cameraId, CameraManager cameraManager, CameraStateCallback stateCallback, Android.OS.Handler backgroundHandler)
+        {
+            var tcs = new TaskCompletionSource<CameraDevice>();
+            var handler = new EventHandler<CameraDevice>((s, e) =>
+            {
+                tcs.SetResult(e);
+            });
+
+            stateCallback.Opened += handler;
+
+            try
+            {
+                cameraManager.OpenCamera(cameraId, stateCallback, backgroundHandler);
+            }
+            catch (Java.IO.IOException e)
+            {
+                throw new System.Exception(e.ToString());
+            }
+
+            var c = await tcs.Task;
+
+            stateCallback.Opened -= handler;
+
+            return c;
+        }
+
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
         {
             Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
 
             base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+
+        protected override async void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.activity_main);
+
+            // set up the camera
+            // get the preview to display video
+            var preview = FindViewById<AutoFitTextureView>(Resource.Id.Preview);
+            var cameraManager = (CameraManager)GetSystemService(CameraService);
+
+            while (!preview.IsAvailable)
+            {
+                await Task.Delay(TimeSpan.FromMilliseconds(100));
+            }
+
+            // get the camera from the camera manager with the given ID
+            var camera = await OpenCameraAsync("0", cameraManager);
+
+            var tcs = new TaskCompletionSource<bool>();
+
+            CameraCaptureSession captureSession = null;
+
+            var failedHandler = new EventHandler<CameraCaptureSession>((s, e) =>
+            {
+                captureSession = e;
+                tcs.TrySetResult(false);
+            });
+
+            var configuredHandler = new EventHandler<CameraCaptureSession>((s, e) =>
+            {
+                captureSession = e;
+                tcs.TrySetResult(true);
+            });
+
+            var sessionCallbackThread = new HandlerThread("SessionCallbackThread");
+            sessionCallbackThread.Start();
+            var handler = new Android.OS.Handler(sessionCallbackThread.Looper);
+
+            var sessionCallback = new CameraSessionCallback();
+            sessionCallback.Configured += configuredHandler;
+            sessionCallback.ConfigureFailed += failedHandler;
+
+            var surface = new Surface(preview.SurfaceTexture);
+
+            var sessionSurfaces = new List<Surface>();
+
+            sessionSurfaces.Add(surface);
+
+            try
+            {
+                camera.CreateCaptureSession(sessionSurfaces, sessionCallback, handler);
+            }
+            catch (Exception e)
+            {
+                tcs.TrySetResult(false);
+                throw e;
+            }
+
+            await tcs.Task;
+
+            var builder = camera.CreateCaptureRequest(CameraTemplate.Preview);
+
+            builder.AddTarget(surface);
+
+            var request = builder.Build();
+
+            captureSession.SetRepeatingRequest(request, null, null);
         }
     }
 }
