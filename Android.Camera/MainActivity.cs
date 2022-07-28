9a5e0b7aa670abe99730d88568370eb1f4bb4634
Ziad <ziad.abdelrahman@subcimaging.com>
2022-07-28 08:55:16 -0230

    Update

--- Android.Camera/MainActivity.cs
+++ Android.Camera/MainActivity.cs
@@ -1,25 +1,135 @@
 ﻿using Android.App;
+using Android.Hardware.Camera2;
+using Android.Media;
 using Android.OS;
+
 using Android.Runtime;
+using Android.Widget;
 using AndroidX.AppCompat.App;
+using Java.IO;
+using System;
+using System.Threading.Tasks;
 
 namespace Android.Camera
 {
     [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
     public class MainActivity : AppCompatActivity
     {
+        /// <summary>
+        /// The location of the SD card in the system.
+        /// </summary>
+        private const string StorageLocation = "/mnt/expand";
+
+        private CameraDevice camera;
+
+        private CameraCaptureSession captureSession;
+
+        private int index = 0;
+
+        private int max = 10000;
+
+        private AutoFitTextureView preview;
+
+        private Button record;
+
+        private MediaRecorder recorder = new MediaRecorder();
+
+        public static Task<CameraDevice> OpenCameraAsync(string cameraId, CameraManager cameraManager)
+        {
+            // create a new background thread to open the camera on
+            var backgroundThread = new HandlerThread("CameraBackground");
+            backgroundThread.Start();
+            var handler = new Android.OS.Handler(backgroundThread.Looper);
+
+            return OpenCameraAsync(cameraId, cameraManager, new CameraStateCallback(), handler);
+        }
+
+        /// <summary>
+        /// Gets a <see cref="CameraDevice"/> from a <see cref="Context"/> and a cameraId.
+        /// </summary>
+        /// <param name="cameraId">Camera Id.</param>
+        /// <param name="cameraManager">A <see cref="CameraManager"/>.</param>
+        /// <param name="stateCallback">A <see cref="CameraStateCallback"/>.</param>
+        /// <param name="backgroundHandler">A background handler.</param>
+        /// <returns>A task of CameraDevice.</returns>
+        public static async Task<CameraDevice> OpenCameraAsync(string cameraId, CameraManager cameraManager, CameraStateCallback stateCallback, Android.OS.Handler backgroundHandler)
+        {
+            var tcs = new TaskCompletionSource<CameraDevice>();
+            var handler = new EventHandler<CameraDevice>((s, e) =>
+            {
+                tcs.SetResult(e);
+            });
+
+            stateCallback.Opened += handler;
+
+            try
+            {
+                cameraManager.OpenCamera(cameraId, stateCallback, backgroundHandler);
+            }
+            catch (Java.IO.IOException e)
+            {
+                throw new System.Exception(e.ToString());
+            }
+
+            var c = await tcs.Task;
+
+            stateCallback.Opened -= handler;
+
+            return c;
+        }
+
+        /// <summary>
+        /// Runs a shell command on the Rayfin.
+        /// </summary>
+        /// <param name="command">The command you wish to execute.</param>
+        /// <param name="timeout">
+        /// The maximum time the command is allowed to run before timing out.
+        /// </param>
+        /// <returns>Anything that comes from stdout.</returns>
+        public static string ShellSync(string command, int timeout = 0)
+        {
+            try
+            {
+                // Run the command
+                var log = new System.Text.StringBuilder();
+                var process = Java.Lang.Runtime.GetRuntime().Exec(new[] { "su", "-c", command });
+                var bufferedReader = new BufferedReader(
+                new InputStreamReader(process.InputStream));
+
+                // Grab the results
+                if (timeout > 0)
+                {
+                    process.Wait(timeout);
+                    return string.Empty;
+                }
+
+                string line;
+
+                while ((line = bufferedReader.ReadLine()) != null)
+                {
+                    log.AppendLine(line);
+                }
+                return log.ToString();
+            }
+            catch
+            {
+                return string.Empty;
+            }
+        }
+
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        {
+            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.activity_main);
-        }
-        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
-        {
-            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
-
-            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
         }
     }
 }
