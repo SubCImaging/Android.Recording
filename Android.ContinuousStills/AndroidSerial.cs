835749afe1aef255d0260a22ac6de8fbe8f70b2d
Adam <apr@subccontrol.com>
2025-08-26 11:12:00 -0230

    SD-1644 Added serial

---
+++ Android.ContinuousStills/AndroidSerial.cs
@@ -0,0 +1,285 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="AndroidSerial.cs" company="SubC Imaging">
+// Copyright (c) SubC Imaging. All rights reserved.
+// </copyright>
+// <author>Adam Rowe</author>
+//-----------------------------------------------------------------------
+
+namespace Android.ContinuousStills
+{
+    using Android.App;
+    using Android.Hardware.Usb;
+    using Hoho.Android.UsbSerial.Driver;
+    using Hoho.Android.UsbSerial.Util;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    /// <summary>
+    /// Responsible for connecting to a USB serial port.
+    /// </summary>
+    public class AndroidSerial
+    {
+        /// <summary>
+        /// Sync to lock.
+        /// </summary>
+        private static readonly object Sync = new object();
+
+        /// <summary>
+        /// USed for permissions.
+        /// </summary>
+        private readonly Activity activity;
+
+        /// <summary>
+        /// Grab serial device from USBManager.
+        /// </summary>
+        private readonly UsbManager usbManager;
+
+        /// <summary>
+        /// Is the device connected.
+        /// </summary>
+        private bool isConnected;
+
+        /// <summary>
+        /// Serial port connected to.
+        /// </summary>
+        private IUsbSerialPort port;
+
+        /// <summary>
+        /// Sending and receiving serial data on.
+        /// </summary>
+        private SerialInputOutputManager serialIoManager;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="AndroidSerial"/> class.
+        /// </summary>
+        /// <param name="activity">Activity used for permission handling.</param>
+        /// <param name="usbManager">Manager to grab device.</param>
+        public AndroidSerial(Activity activity, UsbManager usbManager)
+        {
+            this.activity = activity;
+            this.usbManager = usbManager;
+        }
+
+        /// <summary>
+        /// Fire when data is received from serial port
+        /// </summary>
+        public event EventHandler<string> DataReceived;
+
+        /// <summary>
+        /// Fire when is connected changes
+        /// </summary>
+        public event EventHandler<bool> IsConnectedChanged;
+
+        /// <summary>
+        /// Fire when is sending changes
+        /// </summary>
+        public event EventHandler<bool> IsSendingChanged;
+
+        /// <summary>
+        /// Notify the message router with relevant data
+        /// </summary>
+        public event EventHandler<Notification> Notify;
+
+        /// <summary>
+        /// Gets or sets the string to append to what's being sent.
+        /// </summary>
+        public string Append { get; set; } = string.Empty;
+
+        /// <summary>
+        /// Gets a value indicating whether the port is connected.
+        /// </summary>
+        public bool IsConnected
+        {
+            get
+            {
+                return serialIoManager?.IsOpen ?? false;
+            }
+
+            private set
+            {
+                if (isConnected == value)
+                {
+                    return;
+                }
+
+                isConnected = value;
+                IsConnectedChanged?.Invoke(this, isConnected);
+            }
+        }
+
+        /// <summary>
+        /// Gets a value indicating whether the port is sending information.
+        /// </summary>
+        public bool IsSending
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        /// <summary>
+        /// Gets or sets func to process output.
+        /// </summary>
+        public Func<string, string> Output { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        /// <summary>
+        /// Gets or sets string to prepend to data sent.
+        /// </summary>
+        public string Prepend { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        /// <summary>
+        /// Gets connection status.
+        /// </summary>
+        public string Status => IsConnected ? "Connected" : "Disconnected";
+
+        /// <summary>
+        /// Connect to given address.
+        /// </summary>
+        /// <param name="portName">Port to connect.</param>
+        /// <param name="baudRate">Baud rate to use.</param>
+        /// <returns>True if connected.</returns>
+        public async Task<bool> ConnectAsync(string portName, int baudRate)
+        {
+            var ports = await GetPortsAsync();
+
+            this.port = (from p in ports
+                         where p.ToString() == portName.ToString()
+                         select p).FirstOrDefault();
+
+            if (this.port == null)
+            {
+                return IsConnected = false;
+            }
+
+            serialIoManager = new SerialInputOutputManager(this.port)
+            {
+                BaudRate = baudRate,
+                DataBits = 8,
+                StopBits = StopBits.One,
+                Parity = Parity.None,
+            };
+
+            serialIoManager.DataReceived += (s, e) => SerialIoManager_DataReceived(e);
+
+            try
+            {
+                if (usbManager.HasPermission(port.Driver.Device))
+                {
+                    serialIoManager.Open(usbManager);
+                    return IsConnected = true;
+                }
+
+                var permissionGranted = await usbManager.RequestPermissionAsync(port.Driver.Device, activity.BaseContext);
+                if (permissionGranted)
+                {
+                    serialIoManager.Open(usbManager);
+                }
+                else
+                {
+                    return IsConnected = false;
+                }
+            }
+            catch (Java.IO.IOException e)
+            {
+                return IsConnected = false;
+            }
+
+            return IsConnected = true;
+        }
+
+        /// <summary>
+        /// Set is connected to false.
+        /// </summary>
+        /// <returns>A <see cref="Task"/> representing the asynchronous operation.</returns>
+        public async Task DisconnectAsync()
+        {
+            serialIoManager.Close();
+            IsConnected = false;
+        }
+
+        /// <summary>
+        /// Get all the drivers from the USB manager.
+        /// </summary>
+        /// <returns>Enumerable of serial drivers.</returns>
+        public async Task<IEnumerable<IUsbSerialDriver>> GetDriversAsync() => usbManager.FindAllDriversAsync();
+
+        /// <summary>
+        /// Get all the ports from all the drivers.
+        /// </summary>
+        /// <returns>Enumerable of ports.</returns>
+        public async Task<IEnumerable<IUsbSerialPort>> GetPortsAsync() => (await GetDriversAsync()).SelectMany(d => d.Ports);
+
+        /// <summary>
+        /// Receive data async.
+        /// </summary>
+        /// <returns>A <see cref="Task"/> representing the asynchronous operation.</returns>
+        public Task ReceiveAsync()
+        {
+            throw new Exception();
+        }
+
+        /// <summary>
+        /// Send data to the connect port.
+        /// </summary>
+        /// <param name="bytes">Data to send through the connected port.</param>
+        public void Send(byte[] bytes)
+        {
+            try
+            {
+                port?.Write(bytes, 1000);
+            }
+            catch
+            {
+                // unable to write, is port disconnected?
+            }
+        }
+
+        /// <summary>
+        /// Send data to the connect port.
+        /// </summary>
+        /// <param name="data">Data to send.</param>
+        public void Send(string data)
+        {
+            var d = data + Append;
+
+            Console.WriteLine($"Info: {DateTime.Now:HHmmss.fff} Tx: {d}");
+
+            var dataBytes = Encoding.ASCII.GetBytes(d);
+
+            try
+            {
+                port?.Write(dataBytes, 1000);
+            }
+            catch
+            {
+                // unable to write, is port disconnected?
+            }
+        }
+
+        /// <summary>
+        /// Send data to the connect port.
+        /// </summary>
+        /// <param name="data">Data to send.</param>
+        /// <returns>A <see cref="Task"/> representing the asynchronous operation.</returns>
+        public async Task SendAsync(string data)
+        {
+            // await Task.Delay(1000);
+            Send(data);
+        }
+
+        /// <summary>
+        /// Serial data received handler.
+        /// </summary>
+        /// <param name="e">Data that was received.</param>
+        private void SerialIoManager_DataReceived(SerialDataReceivedArgs e)
+        {
+            var data = Encoding.ASCII.GetString(e.Data);
+
+            Console.WriteLine($"Warning: {DateTime.Now:HHmmss.fff} Rx: {data}");
+        }
+    }
+}
---
+++ Android.ContinuousStills/UsbManagerExtensions.cs
@@ -0,0 +1,84 @@
+﻿// <copyright file="UsbManagerExtensions.cs" company="SubC Imaging">
+// Copyright (c) SubC Imaging. All rights reserved.
+// </copyright>
+
+namespace Android.ContinuousStills
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+    using Android.App;
+    using Android.Content;
+    using Android.Hardware.Usb;
+    using Hoho.Android.UsbSerial.Driver;
+
+    /// <summary>
+    /// Responsible for extending the UsbManager class.
+    /// </summary>
+    public static class UsbManagerExtensions
+    {
+        private const string ACTION_USB_PERMISSION = "com.Hoho.Android.UsbSerial.Util.USB_PERMISSION";
+
+        private static readonly Dictionary<Tuple<Context, UsbDevice>, TaskCompletionSource<bool>> taskCompletionSources =
+            new Dictionary<Tuple<Context, UsbDevice>, TaskCompletionSource<bool>>();
+
+        /// <summary>
+        /// Find all available drivers from current UsbManager and probe all available drivers.
+        /// </summary>
+        /// <param name="usbManager">Usb Manager.</param>
+        /// <returns>List of usb drivers.</returns>
+        public static IList<IUsbSerialDriver> FindAllDriversAsync(this UsbManager usbManager)
+        {
+            // adding a custom driver to the default probe table
+            var table = UsbSerialProber.DefaultProbeTable;
+            table.AddProduct(0x1b4f, 0x0008, Java.Lang.Class.FromType(typeof(CdcAcmSerialDriver))); // IOIO OTG
+
+            table.AddProduct(0x0483, 0x5740, Java.Lang.Class.FromType(typeof(CdcAcmSerialDriver))); // L086
+            var prober = new UsbSerialProber(table);
+
+            return prober.FindAllDrivers(usbManager);
+        }
+
+        /// <summary>
+        /// Request permission to access the device.
+        /// </summary>
+        /// <param name="manager">Usb Manager.</param>
+        /// <param name="device">Device to request permission for.</param>
+        /// <param name="context">Context to request on.</param>
+        /// <returns>A <see cref="Task"/> representing the asynchronous operation.</returns>
+        public static async Task<bool> RequestPermissionAsync(this UsbManager manager, UsbDevice device, Context context)
+        {
+            var completionSource = new TaskCompletionSource<bool>();
+
+            Console.WriteLine($"Device has permission: {manager.HasPermission(device)}");
+
+            var usbPermissionReceiver = new UsbPermissionReceiver(completionSource);
+            context.RegisterReceiver(usbPermissionReceiver, new IntentFilter(ACTION_USB_PERMISSION));
+
+            var intent = PendingIntent.GetBroadcast(context, 0, new Intent(ACTION_USB_PERMISSION), 0);
+            manager.RequestPermission(device, intent);
+
+            return await completionSource.Task;
+        }
+
+        private class UsbPermissionReceiver
+            : BroadcastReceiver
+        {
+            private readonly TaskCompletionSource<bool> completionSource;
+
+            public UsbPermissionReceiver(TaskCompletionSource<bool> completionSource)
+            {
+                this.completionSource = completionSource;
+            }
+
+            public override void OnReceive(Context context, Intent intent)
+            {
+                var device = intent.GetParcelableExtra(UsbManager.ExtraDevice) as UsbDevice;
+                var permissionGranted = intent.GetBooleanExtra(UsbManager.ExtraPermissionGranted, false);
+                context.UnregisterReceiver(this);
+                completionSource.TrySetResult(permissionGranted);
+            }
+        }
+    }
+}
