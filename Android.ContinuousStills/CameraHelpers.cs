268195c3f85364660828e908f4e8e540ca36295c
Adam <apr@subccontrol.com>
2025-08-26 11:12:29 -0230

    SD-1644 Added camera helpers class

---
+++ Android.ContinuousStills/CameraHelpers.cs
@@ -0,0 +1,55 @@
+﻿using Android.Camera;
+using Android.Hardware.Camera2;
+using Android.OS;
+using System;
+using System.Threading.Tasks;
+
+namespace Android.ContinuousStills
+{
+    public class CameraHelpers
+    {
+        public static Task<CameraDevice> OpenCameraAsync(string cameraId, CameraManager cameraManager)
+        {
+            // create a new background thread to open the camera on
+            var backgroundThread = new HandlerThread("CameraBackground");
+            backgroundThread.Start();
+            var handler = new Android.OS.Handler(backgroundThread.Looper);
+
+            return OpenCameraAsync(cameraId, cameraManager, new CameraStateCallback(), handler);
+        }
+
+        /// <summary>
+        /// Gets a <see cref="CameraDevice"/> from a <see cref="Context"/> and a cameraId.
+        /// </summary>
+        /// <param name="cameraId">Camera Id.</param>
+        /// <param name="cameraManager">A <see cref="CameraManager"/>.</param>
+        /// <param name="stateCallback">A <see cref="CameraStateCallback"/>.</param>
+        /// <param name="backgroundHandler">A background handler.</param>
+        /// <returns>A task of CameraDevice.</returns>
+        public static async Task<CameraDevice> OpenCameraAsync(string cameraId, CameraManager cameraManager, CameraStateCallback stateCallback, Android.OS.Handler backgroundHandler)
+        {
+            var tcs = new TaskCompletionSource<CameraDevice>();
+            var handler = new EventHandler<CameraDevice>((s, e) =>
+            {
+                tcs.SetResult(e);
+            });
+
+            stateCallback.Opened += handler;
+
+            try
+            {
+                cameraManager.OpenCamera(cameraId, stateCallback, backgroundHandler);
+            }
+            catch (Java.IO.IOException e)
+            {
+                throw new System.Exception(e.ToString());
+            }
+
+            var c = await tcs.Task;
+
+            stateCallback.Opened -= handler;
+
+            return c;
+        }
+    }
+}
