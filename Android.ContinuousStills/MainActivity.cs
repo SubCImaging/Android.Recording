a4207ec9f4796bceaa8e44cb6049d608bb373810
Adam <apr@subccontrol.com>
2025-08-26 11:13:16 -0230

    SD-1644 Updated to use camera helpers

--- Android.ContinuousStills/MainActivity.cs
+++ Android.ContinuousStills/MainActivity.cs
@@ -53,50 +53,6 @@ namespace Android.ContinuousStills
         /// </summary>
         public StreamConfigurationMap StreamMap { get; private set; }
 
-        public static Task<CameraDevice> OpenCameraAsync(string cameraId, CameraManager cameraManager)
-        {
-            // create a new background thread to open the camera on
-            var backgroundThread = new HandlerThread("CameraBackground");
-            backgroundThread.Start();
-            var handler = new Android.OS.Handler(backgroundThread.Looper);
-
-            return OpenCameraAsync(cameraId, cameraManager, new CameraStateCallback(), handler);
-        }
-
-        /// <summary>
-        /// Gets a <see cref="CameraDevice"/> from a <see cref="Context"/> and a cameraId.
-        /// </summary>
-        /// <param name="cameraId">Camera Id.</param>
-        /// <param name="cameraManager">A <see cref="CameraManager"/>.</param>
-        /// <param name="stateCallback">A <see cref="CameraStateCallback"/>.</param>
-        /// <param name="backgroundHandler">A background handler.</param>
-        /// <returns>A task of CameraDevice.</returns>
-        public static async Task<CameraDevice> OpenCameraAsync(string cameraId, CameraManager cameraManager, CameraStateCallback stateCallback, Android.OS.Handler backgroundHandler)
-        {
-            var tcs = new TaskCompletionSource<CameraDevice>();
-            var handler = new EventHandler<CameraDevice>((s, e) =>
-            {
-                tcs.SetResult(e);
-            });
-
-            stateCallback.Opened += handler;
-
-            try
-            {
-                cameraManager.OpenCamera(cameraId, stateCallback, backgroundHandler);
-            }
-            catch (Java.IO.IOException e)
-            {
-                throw new System.Exception(e.ToString());
-            }
-
-            var c = await tcs.Task;
-
-            stateCallback.Opened -= handler;
-
-            return c;
-        }
-
         public double GetDiskSpaceRemaining()
         {
             var fs = new StatFs(baseDirectory);
@@ -158,7 +114,7 @@ namespace Android.ContinuousStills
             }
 
             // get the camera from the camera manager with the given ID
-            camera = await OpenCameraAsync("0", cameraManager);
+            camera = await CameraHelpers.OpenCameraAsync("0", cameraManager);
             Characteristics = cameraManager.GetCameraCharacteristics("0");
 
             var fpsRangesJava = (Java.Lang.Object[])Characteristics.Get(CameraCharacteristics.ControlAeAvailableTargetFpsRanges);
